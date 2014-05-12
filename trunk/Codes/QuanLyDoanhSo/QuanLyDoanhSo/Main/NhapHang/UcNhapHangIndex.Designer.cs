d41c8b2ed6978385f2e628e97634ef238e2a9b74
giatuc2003@gmail.com <giatuc2003@gmail.com@e088fc05-ab9d-dd04-452e-ea26ee8b6027>
2014-05-12 11:03:30 +0000

    update

---
+++ trunk/Codes/QuanLyDoanhSo/QuanLyDoanhSo/Main/NhapHang/UcNhapHangIndex.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace Weedon
+{
+    partial class UcNhapHangIndex
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnSelect = new System.Windows.Forms.Panel();
+            this.pbKTNK = new System.Windows.Forms.PictureBox();
+            this.lbKTNK = new System.Windows.Forms.Label();
+            this.lbNhapHang = new System.Windows.Forms.Label();
+            this.pbNKNL = new System.Windows.Forms.PictureBox();
+            this.pbNhapHang = new System.Windows.Forms.PictureBox();
+            this.lbNKNL = new System.Windows.Forms.Label();
+            this.pnSelect.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbKTNK)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbNKNL)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbNhapHang)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnSelect
+            // 
+            this.pnSelect.Controls.Add(this.pbKTNK);
+            this.pnSelect.Controls.Add(this.lbKTNK);
+            this.pnSelect.Controls.Add(this.lbNhapHang);
+            this.pnSelect.Controls.Add(this.pbNKNL);
+            this.pnSelect.Controls.Add(this.pbNhapHang);
+            this.pnSelect.Controls.Add(this.lbNKNL);
+            this.pnSelect.Location = new System.Drawing.Point(145, 230);
+            this.pnSelect.Name = "pnSelect";
+            this.pnSelect.Size = new System.Drawing.Size(710, 140);
+            this.pnSelect.TabIndex = 23;
+            // 
+            // pbKTNK
+            // 
+            this.pbKTNK.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pbKTNK.Location = new System.Drawing.Point(569, 10);
+            this.pbKTNK.Name = "pbKTNK";
+            this.pbKTNK.Size = new System.Drawing.Size(100, 100);
+            this.pbKTNK.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbKTNK.TabIndex = 13;
+            this.pbKTNK.TabStop = false;
+            this.pbKTNK.Visible = false;
+            this.pbKTNK.Click += new System.EventHandler(this.pbKTNK_Click);
+            this.pbKTNK.MouseEnter += new System.EventHandler(this.pbKTNK_MouseEnter);
+            this.pbKTNK.MouseLeave += new System.EventHandler(this.pbKTNK_MouseLeave);
+            // 
+            // lbKTNK
+            // 
+            this.lbKTNK.AutoSize = true;
+            this.lbKTNK.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbKTNK.ForeColor = System.Drawing.Color.Gray;
+            this.lbKTNK.Location = new System.Drawing.Point(551, 120);
+            this.lbKTNK.Name = "lbKTNK";
+            this.lbKTNK.Size = new System.Drawing.Size(138, 16);
+            this.lbKTNK.TabIndex = 14;
+            this.lbKTNK.Text = "KIỂM TRA NHẬT KÝ";
+            this.lbKTNK.Visible = false;
+            // 
+            // lbNhapHang
+            // 
+            this.lbNhapHang.AutoSize = true;
+            this.lbNhapHang.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbNhapHang.ForeColor = System.Drawing.Color.Gray;
+            this.lbNhapHang.Location = new System.Drawing.Point(309, 120);
+            this.lbNhapHang.Name = "lbNhapHang";
+            this.lbNhapHang.Size = new System.Drawing.Size(91, 16);
+            this.lbNhapHang.TabIndex = 12;
+            this.lbNhapHang.Text = "NHẬP HÀNG";
+            // 
+            // pbNKNL
+            // 
+            this.pbNKNL.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pbNKNL.Location = new System.Drawing.Point(47, 10);
+            this.pbNKNL.Name = "pbNKNL";
+            this.pbNKNL.Size = new System.Drawing.Size(100, 100);
+            this.pbNKNL.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbNKNL.TabIndex = 11;
+            this.pbNKNL.TabStop = false;
+            this.pbNKNL.Visible = false;
+            this.pbNKNL.Click += new System.EventHandler(this.pbNKNL_Click);
+            this.pbNKNL.MouseEnter += new System.EventHandler(this.pbNKNL_MouseEnter);
+            this.pbNKNL.MouseLeave += new System.EventHandler(this.pbNKNL_MouseLeave);
+            // 
+            // pbNhapHang
+            // 
+            this.pbNhapHang.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pbNhapHang.Location = new System.Drawing.Point(305, 10);
+            this.pbNhapHang.Name = "pbNhapHang";
+            this.pbNhapHang.Size = new System.Drawing.Size(100, 100);
+            this.pbNhapHang.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbNhapHang.TabIndex = 11;
+            this.pbNhapHang.TabStop = false;
+            this.pbNhapHang.Click += new System.EventHandler(this.pbNhapHang_Click);
+            this.pbNhapHang.MouseEnter += new System.EventHandler(this.pbNhapHang_MouseEnter);
+            this.pbNhapHang.MouseLeave += new System.EventHandler(this.pbNhapHang_MouseLeave);
+            // 
+            // lbNKNL
+            // 
+            this.lbNKNL.AutoSize = true;
+            this.lbNKNL.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbNKNL.ForeColor = System.Drawing.Color.Gray;
+            this.lbNKNL.Location = new System.Drawing.Point(52, 120);
+            this.lbNKNL.Name = "lbNKNL";
+            this.lbNKNL.Size = new System.Drawing.Size(90, 16);
+            this.lbNKNL.TabIndex = 12;
+            this.lbNKNL.Text = "NHẬT KÝ NL";
+            this.lbNKNL.Visible = false;
+            // 
+            // UcNhapHangIndex
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.pnSelect);
+            this.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "UcNhapHangIndex";
+            this.Size = new System.Drawing.Size(1000, 600);
+            this.Load += new System.EventHandler(this.UcNhapHangIndex_Load);
+            this.pnSelect.ResumeLayout(false);
+            this.pnSelect.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbKTNK)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbNKNL)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbNhapHang)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnSelect;
+        private System.Windows.Forms.Label lbNhapHang;
+        private System.Windows.Forms.PictureBox pbNKNL;
+        private System.Windows.Forms.PictureBox pbNhapHang;
+        private System.Windows.Forms.Label lbNKNL;
+        private System.Windows.Forms.PictureBox pbKTNK;
+        private System.Windows.Forms.Label lbKTNK;
+
+    }
+}
